f11b8a2149f03a4ccadd20c50a3f5c38c8d30040
darideveloper <darideveloper@gmail.com>
2024-05-10 18:32:16 -0600

    feat (setup): fonts and layout

---
+++ layouts/root-layout.jsx
@@ -0,0 +1,35 @@
+import AOS from 'aos'
+import Head from 'next/head'
+import 'aos/dist/aos.css'
+import { fontRegular } from '@/lib/fonts'
+import { metaData } from '@/lib/meta'
+import { useEffect } from 'react'
+
+
+export default function RootLayout({ children }) {
+  
+  // Load AOS
+  useEffect(() => {
+    AOS.init();
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>{metaData.title}</title>
+        <meta name="description" content={metaData.description} />
+        <meta name="author" content={metaData.author} />
+        <meta name="keywords" content={metaData.keywords} />      
+      </Head>
+      <div className="mx-auto">
+        <header>
+        </header>
+        <main className={fontRegular.className}>
+          {children}
+        </main>
+        <footer>
+        </footer> 
+      </div>
+    </>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,11 @@
 import Image from "next/image"
-import { Inter } from "next/font/google"
 import Hero from "@/sections/hero"
-
-const inter = Inter({ subsets: ["latin"] });
+import RootLayout from "@/layouts/root-layout";
 
 export default function Home() {
   return (
-    <main
-      className={`text-4xl`}
-    >
-      Hero
-    </main>
+    <RootLayout>
+      main content
+    </RootLayout>
   );
 }
