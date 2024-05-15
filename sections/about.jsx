81f4fffbd7bf8164088aec0d8b6395ab5326fd4d
darideveloper <darideveloper@gmail.com>
2024-05-15 14:59:53 -0600

    feat (about): remove unrequired text

--- sections/about.jsx
+++ sections/about.jsx
@@ -25,16 +25,6 @@ export default function About() {
         `}
       >
         We strive to make the BEST TASTING cupcake instead of focusing on cheap, sugary designs like many of the commercial store bakeries do. We DO NOT use fondant (many people are not a fan of it) and we prefer to showcase our designs with better flavors and beautiful piping with our fresh toppings. <br /> <br /> Only the finest, most natural ingredients are used to make our Creations. We're a peanut free HOME kitchen, however, we are NOT Tree nut, gluten free or dairy free operation. <br /> <br /> We use flour, baking powder, sugar, milk, butter, eggs, natural extracts, natural flavors and natural colors in our recipes. (only Watkins natural food dyes are used!) We can also make any cupcake filling with your choice of jelly, mousse or cream 
-        <br /><br />
-        <span
-          className={`
-            font-bold
-            text-pink-dark
-            text-lg
-          `}
-        >
-          We can make liquor or wine infused cupcakes!
-        </span>
       </p>
 
       <Image 
