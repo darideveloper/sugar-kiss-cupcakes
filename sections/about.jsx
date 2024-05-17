637266ed0eae4e614812187ecf4962d06d68a799
darideveloper <darideveloper@gmail.com>
2024-05-17 12:48:15 -0600

    style (about): add minimun order text

--- sections/about.jsx
+++ sections/about.jsx
@@ -25,8 +25,17 @@ export default function About() {
           mx-auto
         `}
       >
-        We strive to make the BEST TASTING cupcake instead of focusing on cheap, sugary designs like many of the commercial store bakeries do. We DO NOT use fondant (many people are not a fan of it) and we prefer to showcase our designs with better flavors and beautiful piping with our fresh toppings. <br /><br /> We use flour, baking powder, sugar, milk, butter, eggs, natural extracts, natural flavors and natural colors in our recipes. (only Watkins natural food dyes are used!) We can also make any cupcake filling with your choice of jelly, mousse or cream 
+        We strive to make the BEST TASTING cupcake instead of focusing on cheap, sugary designs like many of the commercial store bakeries do. We DO NOT use fondant (many people are not a fan of it) and we prefer to showcase our designs with better flavors and beautiful piping with our fresh toppings. <br /><br /> We use flour, baking powder, sugar, milk, butter, eggs, natural extracts, natural flavors and natural colors in our recipes. (only Watkins natural food dyes are used!) We can also make any cupcake filling with your choice of jelly, mousse or cream <br /><br />
+
+        <span
+          className={`
+            font-bold
+          `}
+        >
+          Minimun order is 6 for each flavor
+        </span>
       </p>
+
 
       <Image 
         src="/images/cupcakes-stickers/9.png"
