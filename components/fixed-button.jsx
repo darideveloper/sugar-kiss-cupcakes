16741cd8208f3335a4869bbbcd5b2752fad186b3
daridev <darideveloper@gmail.com>
2024-06-03 12:51:43 -0600

    fix (fixed_button): facebookLink

--- components/fixed-button.jsx
+++ components/fixed-button.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function FixedButton({}) {
   return (
     <a 
-      href="facebookLink"
+      href={facebookLink}
       target="_blank"
       className={`
         fixed-button
